3bb9655405ad4357aa04d80430cb34a739bed434
Shun Miyaki <shunmiyaki@hotmail.com>
2020-10-03 13:22:52 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+201003 / 102_201003-smooth-scrolling-overflows
+- Reviewing implementation of smooth scrolling on overflowed elements
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './css/index.css';
+import DebugGrid from './components/DebugGrid';
+
+function App() {
+  return (
+    <main className="mainContainer">
+      <DebugGrid></DebugGrid>
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <h4>Hello World</h4>
+      <h5>Hello World</h5>
+      <h6>Hello World</h6>
+      <p>Hello World</p>
+    </main>
+  );
+}
+
+export default App;
