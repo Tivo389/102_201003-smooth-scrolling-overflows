31686b98fe702c0b79d2dc791deaa4deb50a0170
Shun Miyaki <shunmiyaki@hotmail.com>
2020-10-10 22:38:38 -0700

    201010 / 201004_activateCurrentShape - Shape in viewport is activated

--- src/components/SmoothScroll.js
+++ src/components/SmoothScroll.js
@@ -7,9 +7,12 @@ import triangle from '../images/triangle.svg';
 class SmoothScroll extends Component {
   // COMPONENT VARIABLES
   imageCount = [];
+  itemStartX = [];
 
   // LIFECYCLE METHODS
-  componentDidMount() {}
+  componentDidMount() {
+    this.getVariables();
+  }
   componentDidUpdate() {}
   componentWillUnmount() {}
 
@@ -18,8 +21,30 @@ class SmoothScroll extends Component {
     for (let i = 0; i < 7; i++) this.imageCount.push(i);
     return (
       <div className="smoothScrollContainer">
+        <div className="itemContainer" onScroll={this.itemContainerOnScroll}>
+          <div className="items">
+            { this.imageCount.map(
+              key => <img src={circle} alt="circle" width="100%" key={key}/>
+            )}
+          </div>
+          <div className="items">
+            { this.imageCount.map(
+              key => <img src={hexagon} alt="hexagon" width="100%" key={key}/>
+            )}
+          </div>
+          <div className="items">
+            { this.imageCount.map(
+              key => <img src={square} alt="square" width="100%" key={key}/>
+            )}
+          </div>
+          <div className="items">
+            { this.imageCount.map(
+              key => <img src={triangle} alt="triangle" width="100%" key={key}/>
+            )}
+          </div>
+        </div>
         <div className="categoryContainer">
-          <div className="category">
+          <div className="category active">
             <img src={circle} alt="circle" width="100%"/>
           </div>
           <div className="category">
@@ -32,23 +57,31 @@ class SmoothScroll extends Component {
             <img src={triangle} alt="triangle" width="100%"/>
           </div>
         </div>
-        <div className="itemContainer">
-          <div className="items">
-            { this.imageCount.map(key => <img src={circle} alt="circle" width="100%" key={key}/>) }
-          </div>
-          <div className="items">
-            { this.imageCount.map(key => <img src={hexagon} alt="hexagon" width="100%" key={key}/>) }
-          </div>
-          <div className="items">
-            { this.imageCount.map(key => <img src={square} alt="square" width="100%" key={key}/>) }
-          </div>
-          <div className="items">
-            { this.imageCount.map(key => <img src={triangle} alt="triangle" width="100%" key={key}/>) }
-          </div>
-        </div>
       </div>
     );
   }
+
+  // FUNCTION
+  getVariables = () => {
+    // itemContainer
+    this.itemContainer = document.querySelector('.itemContainer');
+    this.items = this.itemContainer.querySelectorAll('.items');
+    this.items.forEach(item => this.itemStartX.push(item.getBoundingClientRect().x));
+    this.icScrollWidth = this.itemContainer.scrollWidth;
+    // categoryContainer
+    this.categoryContainer = document.querySelector('.categoryContainer');
+    this.categories = this.categoryContainer.querySelectorAll('.category');
+  };
+  itemContainerOnScroll = (e) => {
+    const detectionThreshold = e.currentTarget.scrollLeft + (window.innerWidth * 0.5);
+    this.itemStartX.forEach((startValue, i) => {
+      const endValue  = (this.itemStartX[i + 1]) ? this.itemStartX[i + 1] : this.icScrollWidth;
+      if (detectionThreshold >= startValue && detectionThreshold < endValue) {
+        this.categories.forEach(category => category.classList.remove('active'));
+        this.categories[i].classList.add('active');
+      }
+    });
+  };
 }
 
 export default SmoothScroll;
